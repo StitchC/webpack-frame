d3e03053aa084843f013ecd0cc932227f75c858e
stitch <979987824@qq.com>
2018-03-09 23:45:56 +0800

    first-commit

---
+++ config/pages.config.js
@@ -0,0 +1,8 @@
+// 注意 这里配置的是html 文件的列表
+// 入口的js 文件也要对应这些html 文件名
+module.exports = {
+    HTMLDirs:[
+        "index",
+        "index2"
+    ]
+}
---
+++ src/script/index.js
@@ -0,0 +1,7 @@
+import output from '../module/output';
+
+
+// 导入css
+import '../style/main.scss';
+
+output();
